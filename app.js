a406254aa074efde06ed4d8aa83dfb61549a4d80
Ann <annachabanets1999@gmail.com>
2019-07-23 15:11:15 +0300

    added percents

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var values = [],
     timer,
     minutes = 0,
-    seconds = 0;
+    seconds = 0,
+    record = Infinity,
+    win = 0,
+    fail = 0,
+    all = 0;
 
 $(document).ready(function(){
 
@@ -53,7 +57,31 @@ function cross(){
 
 function standoff(){
     $('#message').css('display','block').text("Ничья..   Сыграем еще раз?");
+    Percents();
     clearInterval(timer);
+}
+
+function Percents(){
+    let text;
+    switch (all){
+    case 1:  
+        text = `${all} игра сыграна`;
+        break;
+    case 2 :
+        text = `${all} игры сыграно`;
+        break;
+    case 3 :
+        text = `${all} игры сыграно`;
+        break;
+    case 4 :
+        text = `${all} игры сыграно`;
+        break;
+    default:
+            text = `${all} игр сыграно`;
+}
+    $('#all').text(text);
+    let percent = `${(win/all).toPrecision(2) * 100} % побед`;
+    $('#percent').text(percent);
 }
 
 function ComputerChoice(){
@@ -68,16 +96,6 @@ function ComputerChoice(){
                .css('background-image','url(images/cross.png)')
                .css('background-size','cover');
 
-    var flag = 0;
-    values.map(function(a){
-    if (a == 2){
-        flag = 1;
-        }
-    });
-    if (flag == 0){
-    standoff();
-    }else{
-
     if ((values[0] == values[1] && values[1] == values[2] && values[0] == 1) ||
         (values[3] == values[4] && values[4] == values[5] && values[3] == 1) ||
         (values[6] == values[7] && values[7] == values[8] && values[6] == 1) ||
@@ -87,9 +105,22 @@ function ComputerChoice(){
         (values[0] == values[4] && values[4] == values[8] && values[0] == 1) ||
         (values[2] == values[4] && values[4] == values[6] && values[2] == 1)){
         $('#message').css('display','block').text("Вы проиграли!");
+
+        fail++;
+        Percents();
         cross();
         clearInterval(timer);
-    }
+    }else{
+        var flag = 0;
+        values.map(function(a){
+        if (a == 2){
+            flag = 1;
+            }
+        });
+        if (flag == 0){
+        standoff();
+            }
+
 }
 }
 
@@ -104,6 +135,7 @@ $('#start').click(function(){
     seconds = 0;
     $("#sec").text("00");
     $("#min").text("00");
+    all++;
     Timer();
     
     var finish = 0;
@@ -119,15 +151,6 @@ $(".field").click(function(e){
            .css('background-image','url(images/zero.png)')
            .css('background-size','cover');
 
-    var flag = 0;
-    values.map(function(a){
-        if (a == 2){
-            flag = 1;
-        }
-    });
-    if (flag == 0){
-        standoff();
-    }else{
     if ((values[0] == values[1] && values[1] == values[2] && values[0] == 0) ||
         (values[3] == values[4] && values[4] == values[5] && values[3] == 0) ||
         (values[6] == values[7] && values[7] == values[8] && values[6] == 0) ||
@@ -137,13 +160,39 @@ $(".field").click(function(e){
         (values[0] == values[4] && values[4] == values[8] && values[0] == 0) ||
         (values[2] == values[4] && values[4] == values[6] && values[2] == 0)){
             $('#message').css('display','block').text("Вы победили!");
+            win++;
+            Percents();
+            if (minutes*60 + seconds < record){
+                record = minutes*60 + seconds;
+                let time;
+                if (minutes < 10){
+                    time = `0${minutes} :`; 
+                }else{
+                    time = `${minutes} :`;
+                }
+                if (seconds < 10){
+                    time = `${time} 0${seconds}`; 
+                }else{
+                    time = `${time} ${seconds}`;
+                }
+                $('#rec').text(time);
+            }
             cross();
             clearInterval(timer);
         
         }else{
+            var flag = 0;
+            values.map(function(a){
+            if (a == 2){
+                flag = 1;
+                    }
+                });
+            if (flag == 0){
+            standoff();
+            }else{
             setTimeout(ComputerChoice,1000);
-        }
-    }
+                }
+            }
 });
     
    
